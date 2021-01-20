bcecb27fa60133659211999f686b1adc2b3e5ead
Zachary Nylen <znylen@aol.com>
2021-01-19 18:38:49 -0800

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const uri = process.env.MONGODB_URI //|| "mongodb://localhost/stretchcontactdb", { useNewUrlParser: true });
+//const uri = process.env.MONGODB_URI //|| "mongodb://localhost/stretchcontactdb", { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://znylen88:yDqlIYajN5fdExWR@stretchlab.h5wii.mongodb.net/', {dbName: 'userDB'});
 
 app.post("/submit", ({ body }, res) => {
     StretchContact.create(body)
